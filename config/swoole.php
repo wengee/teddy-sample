9966a6d15ac8e801803c81c9b3882c7c1f5844f9
Fung Wing Kit <fengyongjie@vivo.com>
2019-08-26 16:48:31 +0800

    commit

--- config/swoole.php
+++ config/swoole.php
@@ -8,10 +8,24 @@ return [
     'host' => env('SWOOLE_HOST', '0.0.0.0'),
     'port' => (int) env('SWOOLE_PORT', 9500),
 
+    // Websocket config.
+    'websocket' => [
+        'enable' => false,
+        'handler' => null,
+    ],
+
+    // Schedule tasks.
+    // e.g. ['*/5 * * * *', App\Tasks\Demo::class]
     'schedule' => [
     ],
 
-    'dispatch_mode'     => env('SWOOLE_DISPATCH_MODE', 1),
-    'worker_num'        => env('SWOOLE_WORKER_NUM', 1),
-    'task_worker_num'   => env('SWOOLE_TASK_WORKER_NUM', 1),
+    // Custom processes.
+    'processes' => [],
+
+    // Swoole server options.
+    'options' => [
+        'dispatch_mode'     => env('SWOOLE_DISPATCH_MODE', 1),
+        'worker_num'        => env('SWOOLE_WORKER_NUM', 1),
+        'task_worker_num'   => env('SWOOLE_TASK_WORKER_NUM', 1),
+    ],
 ];
