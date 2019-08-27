f74df12a24322763a8f6bbaddc99a843eef3360b
Fung Wing Kit <fengyongjie@vivo.com>
2019-08-27 18:29:12 +0800

    commit

--- dev.sh
+++ dev.sh
@@ -1,22 +1,26 @@
-#!/bin/sh
+#!/bin/bash
 
 start_app()
 {
-    if [ "$PID" = "" ]; then
-        echo -n "Starting server ... "
-        php ./index.php &
-        PID=$!
-        echo "[OK]"
-    fi
+    echo -n "Starting server ... "
+    php ./index.php &
+    PID=$!
+    echo "[OK]"
 }
 
 restart_app()
 {
-    if [ "$PID" = "" ]; then
-        echo "Restart server after 3 seconds."
-        sleep 3
-        start_app
-    fi
+    echo "Restart server after 3 seconds."
+    sleep 3
+}
+
+inotify_wait()
+{
+    inotifywait -e close_write,modify,create,move,delete --format "%w%f" -mrq `pwd` | while read FILE; do
+        if echo $FILE | grep -i -e "\.php$"; then
+            return 0;
+        fi
+    done
 }
 
 sigint_handler()
@@ -27,15 +31,8 @@ sigint_handler()
 
 trap sigint_handler INT TERM
 
-start_app
-inotifywait -e close_write,modify,create,move,delete --format "%w%f" -mrq `pwd` | while read FILE_CHANGED; do
-    if echo $FILE_CHANGED | grep -i -e "\.php$"; then
-        if [ "$PID" != "" ]; then
-            echo -n "Stoping server ... "
-            kill $PID
-            PID=""
-            echo "[OK]"
-            restart_app &
-        fi
-    fi
+while true; do
+    start_app
+    inotify_wait
+    restart_app
 done
