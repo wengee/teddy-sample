ed0157d6f24f0684403b5ff7896ba0c37bb178b9
Fung Wing Kit <fengyongjie@vivo.com>
2019-10-09 11:51:49 +0800

    commit

--- app/Models/ModelBase.php
+++ app/Models/ModelBase.php
@@ -1,7 +1,7 @@
 <?php declare(strict_types=1);
 /**
  * @author   Fung Wing Kit <wengee@gmail.com>
- * @version  2019-10-09 11:47:03 +0800
+ * @version  2019-10-09 11:51:44 +0800
  */
 
 namespace App\Models;
@@ -10,14 +10,14 @@ use Teddy\Model\Model;
 
 abstract class ModelBase extends Model
 {
-    public function save(bool $quiet = true)
+    public function save(bool $quiet = true): bool
     {
         $ret = parent::save($quiet);
         $this->trigger('afterSaveOrDelete');
         return $ret;
     }
 
-    public function delete(bool $quiet = true)
+    public function delete(bool $quiet = true): bool
     {
         $ret = parent::delete($quiet);
         $this->trigger('afterSaveOrDelete');
