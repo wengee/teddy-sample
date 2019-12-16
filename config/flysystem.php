4ee0e5013c0967815fa80b829721b60022596b08
Fung Wing Kit <fengyongjie@vivo.com>
2019-12-16 17:21:16 +0800

    commit

--- config/flysystem.php
+++ config/flysystem.php
@@ -9,22 +9,34 @@ return [
 
     'disks' => [
         'local' => [
-            'driver'     => 'local',
-            'root'       => env('UPLOAD_PATH', '/data/htdocs/uploaded'),
-            'url'        => env('UPLOAD_URL', 'http://lvh.me/uploaded'),
-            'visibility' => 'public',
+            'driver'        => 'local',
+            'root'          => env('UPLOAD_PATH', '/data/htdocs/uploaded'),
+            'url'           => env('UPLOAD_URL', 'http://lvh.me/uploaded'),
+            'visibility'    => 'public',
         ],
 
         'oss' => [
-            'driver'     => 'oss',
-            'access_id'  => env('OSS_ACCESS_ID', ''),
-            'access_key' => env('OSS_ACCESS_KEY', ''),
-            'bucket'     => env('OSS_BUCKET', ''),
-            'endpoint'   => env('OSS_ENDPOINT', ''),
-            'cdnDomain'  => env('OSS_CDN_DOMAIN', ''),
-            'prefix'     => env('OSS_PREFIX', ''),
-            'ssl'        => (bool) env('OSS_SSL', true),
-            'isCName'    => (bool) env('OSS_IS_CNAME', true),
+            'driver'        => 'oss',
+            'access_id'     => env('OSS_ACCESS_ID', ''),
+            'access_key'    => env('OSS_ACCESS_KEY', ''),
+            'bucket'        => env('OSS_BUCKET', ''),
+            'endpoint'      => env('OSS_ENDPOINT', ''),
+            'cdnDomain'     => env('OSS_CDN_DOMAIN', ''),
+            'prefix'        => env('OSS_PREFIX', ''),
+            'ssl'           => (bool) env('OSS_SSL', true),
+            'isCName'       => (bool) env('OSS_IS_CNAME', true),
+        ],
+
+        'cos' => [
+            'driver'        => 'cos',
+            'region'        => env('COS_REGION', ''),
+            'appId'         => env('COS_APP_ID', ''),
+            'secretId'      => env('COS_SECRET_ID', ''),
+            'secretKey'     => env('COS_SECRET_KEY', ''),
+            'bucket'        => env('COS_BUCKET', ''),
+            'cdnDomain'     => env('COS_CDN_DOMAIN', ''),
+            'scheme'        => env('COS_SCHEME', 'http'),
+            'readFromCdn'   => (bool) env('COS_READ_FROM_CDN', false),
         ],
     ],
 ];
