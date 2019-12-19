0766020d2203f0c8f6db60ee7b836b8be6fcea33
Fung Wing Kit <fengyongjie@vivo.com>
2019-12-19 18:16:14 +0800

    commit

--- build/conf/scf.php
+++ build/conf/scf.php
@@ -1,11 +1,12 @@
 <?php declare(strict_types=1);
 /**
  * @author   Fung Wing Kit <wengee@gmail.com>
- * @version  2019-12-19 16:34:29 +0800
+ * @version  2019-12-19 17:11:56 +0800
  */
 
 $options = require __DIR__ . '/production.php';
 $options['output'] = false;
 $options['dist'] = BASE_DIR . '/dist_scf';
 $options['copy']['scf.php'] = 'index.php';
+$options['ignore'][] = 'config/swoole.php';
 return $options;
--- scf.php
+++ scf.php
@@ -1,19 +1,21 @@
 <?php declare(strict_types=1);
 /**
  * @author   Fung Wing Kit <wengee@gmail.com>
- * @version  2019-12-19 16:21:16 +0800
+ * @version  2019-12-19 17:09:41 +0800
  */
 
 defined('BASE_PATH') || define('BASE_PATH', __DIR__ . '/');
 defined('IN_SCF') || define('IN_SCF', true);
 
 require BASE_PATH . 'vendor/autoload.php';
+Teddy\Guzzle\DefaultHandler::set('curl');
 
 function main($event, $context)
 {
     static $app;
     if (!isset($app)) {
         $app = require BASE_PATH . 'bootstrap/app.php';
+        $app->addStaticFileMiddleware(BASE_PATH . 'public');
     }
 
     return $app->run($event, $context);
