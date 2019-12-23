d073c17a30cc7c705a2c62b08ea825b22765b8c5
Fung Wing Kit <fengyongjie@vivo.com>
2019-12-23 15:18:25 +0800

    commit

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -34,3 +34,44 @@ if (!function_exists('validate')) {
         return $instance->validate($data, $rules, $quiet);
     }
 }
+
+if (!function_exists('dec2any')) {
+    function dec2any(int $num, $base = 62): string
+    {
+        if (is_string($base)) {
+            $index = $base;
+            $base = strlen($index);
+        } else {
+            $base = (int) $base;
+            $index = substr('l2Vj5aUOBCLpdFRsK6ytAXzGbY1QWewvHhxE4gT38SPqmfioc7Ju9NDr0IZMkn', 0, $base);
+        }
+
+        $out = '';
+        for ($t = floor(log10($num) / log10($base)); $t >= 0; $t--) {
+            $a = intval(floor($num /  $base** $t));
+            $out = $out . substr($index, $a, 1);
+            $num = $num - ($a *  $base** $t);
+        }
+        return $out;
+    }
+}
+
+if (!function_exists('any2dec')) {
+    function any2dec(string $num, $base = 62): int
+    {
+        if (is_string($base)) {
+            $index = $base;
+            $base = strlen($index);
+        } else {
+            $base = (int) $base;
+            $index = substr('l2Vj5aUOBCLpdFRsK6ytAXzGbY1QWewvHhxE4gT38SPqmfioc7Ju9NDr0IZMkn', 0, $base);
+        }
+
+        $out = 0;
+        $len = strlen($num) - 1;
+        for ($t = 0; $t <= $len; $t++) {
+            $out = $out + strpos($index, substr($num, $t, 1)) *  $base**($len - $t);
+        }
+        return (int) $out;
+    }
+}
