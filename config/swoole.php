893d9033dd77d0f52f423c4850ff20664e622085
Fung Wing Kit <wengee@gmail.com>
2021-04-28 10:26:38 +0800

    commit

--- config/swoole.php
+++ config/swoole.php
@@ -1,8 +1,18 @@
 <?php declare(strict_types=1);
 /**
  * @author   Fung Wing Kit <wengee@gmail.com>
- * @version  2019-11-23 10:30:00 +0800
+ * @version  2021-04-28 10:24:24 +0800
  */
+
+$scheduleEnabled = (bool) env('SWOOLE_SCHEDULE_ENABLE');
+$reactorNum      = (int) env('SWOOLE_REACTOR_NUM', 1);
+$workerNum       = (int) env('SWOOLE_WORKER_NUM', 1);
+$taskWorkerNum   = (int) env('SWOOLE_TASK_WORKER_NUM', 1);
+if ($scheduleEnabled) {
+    $reactorNum    = (int) env('SWOOLE_SHEDULE_REACTOR_NUM', $reactorNum);
+    $workerNum     = (int) env('SWOOLE_SHEDULE_WORKER_NUM', $workerNum);
+    $taskWorkerNum = (int) env('SWOOLE_SHEDULE_TASK_WORKER_NUM', $taskWorkerNum);
+}
 
 return [
     'host' => env('SWOOLE_HOST', '0.0.0.0'),
@@ -10,14 +20,23 @@ return [
 
     // Websocket config.
     'websocket' => [
-        'enable' => false,
-        'handler' => null,
+        'enabled'  => false,
+        'handler'  => null,
     ],
 
     // Schedule tasks.
     // time slots: second minute hour day month week
     // e.g. ['*/5 * * * *', App\Tasks\Demo::class]
     'schedule' => [
+        'enabled' => $scheduleEnabled,
+        'list'    => [
+        ],
+    ],
+
+    // Task queue config.
+    'queue' => [
+        'enabled'  => false,
+        'consumer' => (bool) env('SWOOLE_QUEUE_ENABLE'),
     ],
 
     // Custom processes.
@@ -25,11 +44,11 @@ return [
 
     // Swoole server options.
     'options' => [
-        'reactor_num'           => (int) env('SWOOLE_REACTOR_NUM', 1),
-        'worker_num'            => (int) env('SWOOLE_WORKER_NUM', 1),
-        'task_worker_num'       => (int) env('SWOOLE_TASK_WORKER_NUM', 1),
+        'reactor_num'           => $reactorNum,
+        'worker_num'            => $workerNum,
+        'task_worker_num'       => $taskWorkerNum,
         'dispatch_mode'         => (int) env('SWOOLE_DISPATCH_MODE', 1),
-        'log_file'              => env('SWOOLE_LOG_FILE', __DIR__ . '/../runtime/swoole.log'),
+        'log_file'              => env('SWOOLE_LOG_FILE', __DIR__.'/../runtime/swoole.log'),
         'log_level'             => (int) env('SWOOLE_LOG_LEVEL', SWOOLE_LOG_DEBUG),
         'max_connection'        => (int) env('SWOOLE_MAX_CONNECTION', 100),
         'http_parse_post'       => false,
